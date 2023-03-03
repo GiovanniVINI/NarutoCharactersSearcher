cbf384daa43dc5d047ea232a914f6976680b92af
vinicius_giovanni <giovannivini@hotmail.com>
2023-03-03 17:31:11 -0300

    feat: adding features :rocket:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Apollo, gql } from 'apollo-angular';
+import { CardsComponent } from './components/cards/cards.component';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,65 @@ import { Apollo, gql } from 'apollo-angular';
   styleUrls: ['./app.component.css'],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   characters: any;
   villages: any;
 
-  constructor(private apollo: Apollo) {}
+  pageCurrent = 1
 
+  constructor(private readonly apollo: Apollo, private readonly matDialog: MatDialog ) {
+
+  }
+
+  
+
+  onOpenDialogClick(character: any){
+    this.matDialog.open(CardsComponent, { 
+      data: { 
+        character
+      }
+    }); 
+  }
+
+  onScroll(){ 
+    this.apollo
+      .query<{
+        characters: { results: any[]},
+        villages: { results: any[]} 
+      }>({
+        query: gql`
+          query ($page: Int){
+            characters(page: $page) {
+              info {
+                count
+                pages
+                next
+                prev
+              }
+              results {
+                _id
+                name
+                avatarSrc
+                description
+                rank
+                village
+             }
+            }
+          villages {
+              results{ 
+              name}
+            }
+          }
+        `,
+        variables: {
+          page: this.pageCurrent + 1
+        }
+      })
+      .subscribe((result) => {
+        this.characters = [...this.characters, ...result.data.characters.results];
+        this.pageCurrent += 1
+      });
+  }
   ngOnInit() {
     this.apollo
       .query<{
@@ -30,7 +85,9 @@ export class AppComponent {
               }
               results {
                 _id
+                rank
                 name
+                age
                 avatarSrc
                 description
                 rank
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { AppComponent } from './app.component';
-
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
+import { MatDialogModule } from  "@angular/material/dialog"
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
+import { GraphQLModule } from './graphql.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const uri = 'https://narutoql.up.railway.app/graphql'; // URL da API GraphQL
 
@@ -23,7 +26,11 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   imports: [
     BrowserModule,
     HttpClientModule,
-    ApolloModule
+    ApolloModule,
+    InfiniteScrollModule,
+    GraphQLModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [ {
     provide: APOLLO_OPTIONS,
